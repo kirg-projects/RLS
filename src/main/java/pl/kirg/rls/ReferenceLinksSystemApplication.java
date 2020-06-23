b9aecf5be895031f0f2c96d327b355cb1f31ed34
born-tocode <pl.borntocode@gmail.com>
2020-06-23 14:24:38 +0200

    clean build

---
+++ src/main/java/pl/kirg/rls/ReferenceLinksSystemApplication.java
@@ -0,0 +1,15 @@
+package pl.kirg.rls;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReferenceLinksSystemApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(ReferenceLinksSystemApplication.class, args);
+    }
+
+}
---
+++ src/main/java/pl/kirg/rls/config/DataSourceConfig.java
@@ -0,0 +1,23 @@
+package pl.kirg.rls.config;
+
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DataSourceConfig
+{
+    @Bean
+    public DataSource getDataSource()
+    {
+        return DataSourceBuilder
+                .create()
+                .driverClassName("org.sqlite.JDBC")
+                .url("jdbc:sqlite:./target/rlsdb.db")
+                .username("SA")
+                .password("")
+                .build();
+    }
+}
