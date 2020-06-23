1fd9cf544c9fb73ce4570e16b071286184b56ed2
born-tocode <pl.borntocode@gmail.com>
2020-06-23 17:46:29 +0200

    clean build

---
+++ src/test/java/pl/kirg/rls/ReferenceLinksSystemApplicationTests.java
@@ -0,0 +1,15 @@
+package pl.kirg.rls;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReferenceLinksSystemApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
