18e4c3e9ad1a3ba94a84a339df019f0b11cddf55
Mateusz Boguszewski <pl.borntocode@gmail.com>
2020-07-11 01:04:29 +0200

    basic web security

---
+++ src/main/java/pl/kirg/rls/security/WebSecurityConfig.java
@@ -0,0 +1,29 @@
+package pl.kirg.rls.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter
+{
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception
+    {
+        http.authorizeRequests()
+            .antMatchers("/shop")
+            .hasRole("USER")
+            .and()
+            .formLogin()
+            .loginPage("/login")
+            .successForwardUrl("/shop");
+
+        http.authorizeRequests()
+            .antMatchers("/admin")
+            .hasRole("ADMIN");
+    }
+
+}
