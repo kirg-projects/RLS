113e0eacc78f17f2e8fa391ee14ef9322abf3812
Mateusz Boguszewski <pl.borntocode@gmail.com>
2020-07-11 02:01:42 +0200

    test for login page

--- src/test/java/pl/kirg/rls/security/WebSecurityConfigTest.java
+++ src/test/java/pl/kirg/rls/security/WebSecurityConfigTest.java
@@ -3,12 +3,16 @@ package pl.kirg.rls.security;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.log;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 @WebMvcTest
 public class WebSecurityConfigTest
@@ -20,12 +24,22 @@ public class WebSecurityConfigTest
     private MockMvc mockMvc;
 
     @Test
+    public void loginPageExists() throws Exception
+    {
+        mockMvc.perform(get(LOGIN_URL))
+               .andExpect(model().hasNoErrors())
+               .andExpect(view().name("login"))
+               .andExpect(content().contentType(MediaType.TEXT_HTML))
+               .andDo(log());
+    }
+
+    @Test
     public void accessToShopRedirection() throws Exception
     {
         mockMvc.perform(get("/shop"))
                .andExpect(status().isFound())
                .andExpect(header().stringValues("Location", LOGIN_URL))
-               .andDo(print());
+               .andDo(log());
     }
 
     @Test
@@ -34,6 +48,6 @@ public class WebSecurityConfigTest
         mockMvc.perform(get("/admin"))
                .andExpect(status().isFound())
                .andExpect(header().stringValues("Location", LOGIN_URL))
-               .andDo(print());
+               .andDo(log());
     }
 }
