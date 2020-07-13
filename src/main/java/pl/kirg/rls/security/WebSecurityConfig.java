1c09dc5894525bac9fca001b15f103625d3be56e
Mateusz Boguszewski <pl.borntocode@gmail.com>
2020-07-13 22:52:54 +0200

    basic http security but isn't the end

--- src/main/java/pl/kirg/rls/security/WebSecurityConfig.java
+++ src/main/java/pl/kirg/rls/security/WebSecurityConfig.java
@@ -1,9 +1,11 @@
 package pl.kirg.rls.security;
 
+import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
 @Configuration
 @EnableWebSecurity
@@ -13,17 +15,35 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter
     @Override
     protected void configure(HttpSecurity http) throws Exception
     {
+
+        http.authorizeRequests()
+            .antMatchers("/")
+            .permitAll();
+
+        http.sessionManagement()
+            .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
+            .maximumSessions(1)
+            .maxSessionsPreventsLogin(true)
+            .expiredUrl("/expired");
+
         http.authorizeRequests()
             .antMatchers("/shop")
             .hasRole("USER")
             .and()
             .formLogin()
             .loginPage("/login")
-            .successForwardUrl("/shop");
+            .loginProcessingUrl("/login")
+            .and()
+            .httpBasic();
 
         http.authorizeRequests()
             .antMatchers("/admin")
-            .hasRole("ADMIN");
+            .hasRole("ADMIN")
+            .and()
+            .authorizeRequests()
+            .requestMatchers(EndpointRequest.toAnyEndpoint())
+            .hasRole("ENDPOINT_ADMIN")
+            .and()
+            .httpBasic();
     }
-
 }
