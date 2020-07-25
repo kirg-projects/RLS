7d8e3dd44c695f8a72c9ca2eeab39b93298a2273
Kamil Maksymowicz <maxio090124@interia.pl>
2020-07-25 20:45:42 +0200

    Create simple controller

---
+++ src/main/java/pl/kirg/rls/controller/HomeController.java
@@ -0,0 +1,22 @@
+package pl.kirg.rls.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequestMapping(value = "/home")
+public class HomeController {
+
+
+    @GetMapping
+    public String home(Model model){
+        model.addAttribute("name","HomeModel"); //TODO
+        return "vHome"; //TODO
+    }
+
+
+
+}
