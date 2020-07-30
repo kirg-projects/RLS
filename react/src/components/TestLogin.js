7097370180181f9cb4e6b15e8ed7589efeb6e800
mateusz <biskup.mateusz1997@wp.pl>
2020-07-31 00:09:58 +0200

    Basic routing with test components

---
+++ react/src/components/TestLogin.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+class TestLogin extends React.Component {
+    render() {
+        return (
+            <h1>This is test string from TestLogin</h1>
+        )
+    }
+}
+
+export default TestLogin
