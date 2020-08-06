346288d582fb6ed5c45c9bda0a250dbe3c651e76
franek <franciszekgrobelny@gmail.com>
2020-08-06 13:30:38 +0200

    initial registration template

--- react/src/components/Registration.js
+++ react/src/components/Registration.js
@@ -1,9 +1,193 @@
 import React from 'react'
+import {Typography,TextField, Button,FormControl,FormControlLabel,Grid,Link,Checkbox,NativeSelect,FormHelperText,InputLabel, Select  } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
 
 class Registration extends React.Component {
+    constructor(){
+        super(),
+        this.state={
+
+        }
+    }
+
     render() {
         return (
-            <h1>This is  Registration</h1>
+            <div>
+                <Typography component="h1" variant="h5" color="primary" align="center">
+                    Register
+                </Typography>
+                   <form noValidate>
+                       <FormControl fullWidth>
+                           <InputLabel htmlFor="age-native-simple">Account type</InputLabel>
+                           <Select
+                             native
+    //                       value={state.??}
+    //                       onChange={handleChange}
+//                           inputProps={{
+//                             name: 'age',
+//                             id: 'age-native-simple',
+//                           }}
+                           >
+                             <option aria-label="None" value="" />
+                             <option value={"osobaFizyczna"}>osoba fizyczna</option>
+                             <option value={"osobaFizycznaZDzielanoscia"}>osoba fizyczna z działalnością gospodarczą</option>
+                             <option value={"spolka"}>spółka</option>
+                             <option value={"zwykly"}>zwykły użytkownik</option>
+                             <option value={"opiekun"}>opiekun</option>
+                           </Select>
+                       </FormControl>
+                       <TextField
+                          variant="outlined"
+                          margin="normal"
+                          required
+                          fullWidth
+                          id="firstName"
+                          type="text"
+                          label="First Name"
+                          name="firstName"
+                       />
+                       <TextField
+                          variant="outlined"
+                          margin="normal"
+                          required
+                          fullWidth
+                          id="lastName"
+                          type="text"
+                          label="Last Name"
+                          name="lastName"
+                       />
+{/* TODO label nie może nachodzić na typ inputa */}
+                       <TextField
+                          variant="outlined"
+                          margin="normal"
+                          required
+                          fullWidth
+                          id="birthDate"
+                          type="date"
+                          label="Birth Date"
+                          name="birthDate"
+                       />
+                       <TextField
+                          variant="outlined"
+                          margin="normal"
+                          required
+                          fullWidth
+                          id="country"
+                          type="text"
+                          label="Country"
+                          name="country"
+                       />
+                       <TextField
+                          variant="outlined"
+                          margin="normal"
+                          required
+                          fullWidth
+                          id="street"
+                          type="text"
+                          label="Street"
+                          name="street"
+                       />
+                       <TextField
+                          variant="outlined"
+                          margin="normal"
+                          required
+                          fullWidth
+                          id="flatNumber"
+                          type="number"
+                          label="Flat Number"
+                          name="flatNumber"
+                       />
+{/* TODO regex do kodu pocztowego */}
+                       <TextField
+                          variant="outlined"
+                          margin="normal"
+                          required
+                          fullWidth
+                          id="zip"
+                          type="number"
+                          label="ZIP"
+                          name="zip"
+                       />
+                       <TextField
+                          variant="outlined"
+                          margin="normal"
+                          required
+                          fullWidth
+                          id="city"
+                          type="text"
+                          label="City"
+                          name="city"
+                       />
+{/* TODO regex dla numeru telefonicznego */}
+                       <TextField
+                          variant="outlined"
+                          margin="normal"
+                          required
+                          fullWidth
+                          id="phone"
+                          type="number"
+                          label="Phone"
+                          name="phone"
+                       />
+                       <TextField
+                           variant="outlined"
+                           margin="normal"
+                           required
+                           fullWidth
+                           id="email"
+                           label="Email Address"
+                           type="email"
+                           name="email"
+                           autoComplete="email"
+                       />
+                       <TextField
+                          variant="outlined"
+                          margin="normal"
+                          required
+                          fullWidth
+                          id="reEmail"
+                          label="Repeat Email Address"
+                          type="email"
+                          name="reEmail"
+                          autoComplete="email"
+                      />
+                       <TextField
+                           variant="outlined"
+                           margin="normal"
+                           required
+                           fullWidth
+                           name="password"
+                           label="Password"
+                           type="password"
+                           id="password"
+
+                       />
+                       <TextField
+                          variant="outlined"
+                          margin="normal"
+                          required
+                          fullWidth
+                          name="rePassword"
+                          label="Repeat password"
+                          type="password"
+                          id="rePassword"
+
+                      />
+                       <FormControlLabel
+                           control={<Checkbox value="remember" color="primary" />}
+                           label="Remember me"
+                       />
+                       <Button
+                           type="submit"
+                           fullWidth
+                           variant="contained"
+                           color="primary"
+                       >
+                           Sign up
+                       </Button>
+                   </form>
+               </div>
         )
     }
 }
