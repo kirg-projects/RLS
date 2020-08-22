a5cc58a176b85a8e3c49284ed934d26333092eb5
Mateusz Boguszewski <pl.borntocode@gmail.com>
2020-08-22 12:05:14 +0200

    User repository with some essential methods

---
+++ src/main/java/pl/kirg/rls/repository/UserRepository.java
@@ -0,0 +1,21 @@
+package pl.kirg.rls.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import pl.kirg.rls.User;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, String>
+{
+
+    Optional<User> findByUsername(String username);
+
+    Iterable<? extends User> findUserByCustomerLastNameAndEnabledIsTrue(String lastName);
+
+    Optional<User> findUserByCustomerEmail(String email);
+
+    Optional<User> findUserByCustomerPhoneNumber(String phoneNumber);
+}
