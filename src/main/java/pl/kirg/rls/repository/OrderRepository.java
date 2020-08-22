14a4ffc4b0f1578425e5ddc463119c4d019f3ef3
Mateusz Boguszewski <pl.borntocode@gmail.com>
2020-08-22 13:44:14 +0200

    just order repository without additional methods

---
+++ src/main/java/pl/kirg/rls/repository/OrderRepository.java
@@ -0,0 +1,12 @@
+package pl.kirg.rls.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import pl.kirg.rls.Order;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long>
+{
+
+}
