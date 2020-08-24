fabee041324a1ff67fe7ac7f96b83492bb010879
Mateusz Boguszewski <pl.borntocode@gmail.com>
2020-08-24 13:48:22 +0200

    relationship to Customer

--- src/main/java/pl/kirg/rls/Address.java
+++ src/main/java/pl/kirg/rls/Address.java
@@ -10,6 +10,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
@@ -24,20 +25,28 @@ public class Address
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
     @NotNull
     @Size(min = 2, max = 40)
     private String country;
+
     @NotNull
     @Size(min = 2, max = 60)
     private String street;
+
     @NotNull
     @Size(min = 1, max = 20)
     private String streetNumber;
+
     @NotNull
     @NumberFormat
     @Size(min = 4, max = 10)
     private String postCode;
+
     @Size(min = 2, max = 40)
     private String city;
+
+    @ManyToOne(targetEntity = Customer.class)
+    private Customer customer;
 
 }
