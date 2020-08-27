b159ba781b20a7b96a437875cc87796c08054aba
Mateusz Boguszewski <pl.borntocode@gmail.com>
2020-08-27 16:25:56 +0200

    timestamp property

--- src/main/java/pl/kirg/rls/domain/Payment.java
+++ src/main/java/pl/kirg/rls/domain/Payment.java
@@ -4,11 +4,17 @@ import lombok.AccessLevel;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.sql.Timestamp;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Data
@@ -18,11 +24,19 @@ public class Payment
 {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Enumerated(EnumType.STRING)
-    @Column(name = "status")
+    @Column(name = "status", nullable = false)
     private Status paymentStatus;
+
+    @Column(name = "response", nullable = false)
+    private String responseFromProvider;
+
+    @CreationTimestamp
+    @DateTimeFormat
+    @Column(nullable = false, updatable = false)
+    private final Timestamp timestamp;
 
 }
