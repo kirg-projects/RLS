eb8e6c20fba552129dc2d2dc46f9ee0f33bdd93b
Mateusz Boguszewski <pl.borntocode@gmail.com>
2020-08-27 16:19:18 +0200

    useless relation

--- src/main/java/pl/kirg/rls/domain/Address.java
+++ src/main/java/pl/kirg/rls/domain/Address.java
@@ -11,7 +11,6 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
@@ -48,8 +47,5 @@ public class Address
 
     @Size(min = 2, max = 40)
     private String city;
-
-    @ManyToOne(targetEntity = Customer.class)
-    private Customer customer;
 
 }
