64912a838e34f088029eb6e7693c7ff4b8dcd3b3
Mateusz Boguszewski <pl.borntocode@gmail.com>
2020-08-29 14:45:29 +0200

    updated parametrized type for JpaRepository for ID as Long not String

--- src/main/java/pl/kirg/rls/repository/UserRepository.java
+++ src/main/java/pl/kirg/rls/repository/UserRepository.java
@@ -8,7 +8,7 @@ import pl.kirg.rls.domain.User;
 import java.util.Optional;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, String>
+public interface UserRepository extends JpaRepository<User, Long>
 {
 
     Optional<User> findByUsername(String username);
