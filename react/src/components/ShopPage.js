a3239ac204a20e8c743fdf26a4b0c4f194b39f67
Wojciech <wojciech.lupina1@gmail.com>
2020-09-05 15:05:54 +0200

    Działa wyszukiwanie!

--- react/src/components/ShopPage.js
+++ react/src/components/ShopPage.js
@@ -5,14 +5,6 @@ import { InputBase, Paper, IconButton, Typography } from '@material-ui/core';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/styles';
 
-function handleSearch(event) {
-    if(event.key === 'Enter'){
-        console.log('Send search request...')
-        // send with GET method
-        // update content
-    }
-}
-
 function toStars(n) {
     let result = [];
     let i = 0;
@@ -95,7 +87,6 @@ class ShopPage extends React.Component {
             'http://127.0.0.1:3000/courses.json',
         )
         .then(response => {
-            console.log(`Status code: ${response.status}`);
             this.setState({
                 courses: response.data.items
             });
@@ -104,6 +95,7 @@ class ShopPage extends React.Component {
                 return item.imgPath;
             })
 
+            let coursesCopy = this.state.courses.slice();
             for(let i = 0; i<imgPaths.length; i++) {
                 axios.get(imgPaths[i], { responseType: 'arraybuffer' })
                     .then(imgRes => {
@@ -113,13 +105,51 @@ class ShopPage extends React.Component {
                                 '',
                             )
                         );
-                        let coursesCopy = this.state.courses.slice();
                         coursesCopy[i].img = "data:;base64," + base64;
                         this.setState({ courses: coursesCopy });
                     }
                 );
             }
+            for(let i = 0; i<response.data.items.length; i++) {
+                coursesCopy[i].ptr = React.createRef();
+            }
+            this.setState({ courses: coursesCopy });
         });
+        this.handleSearch = this.handleSearch.bind(this);
+    }
+
+    handleSearch(event) {
+        if(event.key === 'Enter'){
+            if(event.target.value == '') {
+                for (let i = 0; i<this.state.courses.length; i++) {
+                    this.state.courses[i].ptr.current.style.display = 'block';
+                }
+            }
+
+            let coursesLen = this.state.courses.length;
+            let coursesToHide = Array(coursesLen).fill(false);
+
+            let titles = this.state.courses.map(course => {
+                return course.title;
+            });
+
+            let descriptions = this.state.courses.map(course => {
+                return course.description;
+            });
+
+            for (let i = 0; i<coursesLen; i++) {
+                if ( !(new RegExp(event.target.value, "i").test(titles[i])) &&
+                     !(new RegExp(event.target.value, "i").test(descriptions[i])) ) {
+                    coursesToHide[i] = true;
+                }
+            }
+            for(let i = 0; i<coursesToHide.length; i++) {
+                console.log(coursesToHide[i]);
+                if (coursesToHide[i]) {
+                    this.state.courses[i].ptr.current.style.display = 'none';
+                }
+            }
+        }
     }
 
     render() {
@@ -132,11 +162,11 @@ class ShopPage extends React.Component {
                     <InputBase
                         placeholder="Search…"
                         className={classes.input}
-                        onKeyPress={handleSearch}
+                        onKeyPress={this.handleSearch}
                     />
                 </div>
                 { this.state.courses.map((elem, key) => (
-                    <div key={key}>
+                    <div key={key} ref={elem.ptr}>
                         <Paper className={classes.offer} elevation={3}>
                             <img src={elem.img} className={classes.logo} />
                             <div style={{margin: 10, marginLeft: 0, width: '100%'}}>
