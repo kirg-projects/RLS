09e339df58941c538f571dc5bc97e936c9bd421b
barteqPC <barteq2011@gmail.com>
2020-12-21 21:11:59 +0100

    Manager entity done, projections for new Company entity, repositories and services for CRUD operations.

---
+++ src/main/java/pl/kirg/rls/domain/Manager.java
@@ -0,0 +1,50 @@
+package pl.kirg.rls.domain;
+
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Data
+@Entity
+@Table(name = "manager")
+@NoArgsConstructor(access = AccessLevel.PUBLIC, force = true)
+@Getter
+@Setter
+public class Manager
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @OneToOne(cascade = {
+            CascadeType.DETACH,
+            CascadeType.MERGE,
+            CascadeType.PERSIST,
+            CascadeType.REFRESH})
+    @JoinColumn(name = "user_id")
+    private User user;
+    // @ManyToOne(fetch = FetchType.LAZY)
+    // private List<Company> companies;
+
+    @Override
+    public String toString()
+    {
+        return "Manager{" +
+               "id=" + id +
+               ", user=" + user +
+               '}';
+    }
+}
